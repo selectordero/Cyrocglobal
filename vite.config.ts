e3b738972e0b68dea8d7d8bff6aac9a1657e4b1b
selectordero <derickisoe00@gmail.com>
2025-08-29 16:28:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cyrocglobal/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
